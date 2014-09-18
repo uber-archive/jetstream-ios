cf193f3f6f5476917ebdffd92f4579deb6a3512b
Tuomas Artman <tuomas@uber.com>
2014-09-18 11:45:58 -0700

    Initial Commit

---
+++ Jetstream/Jetstream.h
@@ -0,0 +1,19 @@
+//
+//  Jetstream.h
+//  Jetstream
+//
+//  Created by Tuomas Artman on 9/18/14.
+//  Copyright (c) 2014 Uber. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Jetstream.
+FOUNDATION_EXPORT double JetstreamVersionNumber;
+
+//! Project version string for Jetstream.
+FOUNDATION_EXPORT const unsigned char JetstreamVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Jetstream/PublicHeader.h>
+
+
