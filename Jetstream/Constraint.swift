de9026fdf1bf0b120d00fcdfc9d92dd5239bf1cf
Tuomas Artman <tuomas@uber.com>
2015-01-09 17:43:35 -0800

    Fixed constraint initializer

--- Jetstream/Constraint.swift
+++ Jetstream/Constraint.swift
@@ -28,10 +28,8 @@ import Foundation
 /// The has new value property constraint is used to describe a property value has a new value.  That is that this
 /// property has a new value described by the change.
 public class HasNewValuePropertyConstraint {
-    public class HasNewValuePropertyConstraint {
-        public init() {
-            // No-op
-        }
+    public init() {
+        // No-op
     }
 }
 
